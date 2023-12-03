a3504d94906b3967e613fb4855f38ecf91424e6e
tw-blue <toniwalter.blue@gmail.com>
2023-12-03 22:18:43 +0100

    Forgot

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getDatabase } from 'firebase-admin/database';
 import connectSessionFirebase from 'connect-session-firebase';
+import { Firestore } from '@google-cloud/firestore';
 const FirebaseStore = connectSessionFirebase(session);
 
 const importJson = (filename) => JSON.parse(
@@ -26,9 +27,13 @@ firebaseConfig["credential"] = firebase.credential.cert(serviceAccount);
 
 // Initialize Firebase Admin SDK
 initializeApp(firebaseConfig);
+const auth = getAuth();
 
+const firestore = new Firestore({
+  projectId: 'tcc-1-ev-intern'
+});
 
-
+//Initialize Webserver
 const app = express();
 app.set("trust proxy", 1);
 app.use(cookieParser());
@@ -50,7 +55,7 @@ app.use(csrfSynchronisedProtection);
 
 const authenticateJWT = (req, res, next) => {
   const sessionCookie = req.cookies['firebase-session'] || '';
-  getAuth()
+  auth
     .verifySessionCookie(sessionCookie, true)
     .then((decodedToken) => {
       req.token = decodedToken;
@@ -77,20 +82,62 @@ const verifyClaimsIntern = (claims, req, res, next) => {
 }
 
 app.use(express.static("static", { index: false, extensions: ['html'] }));
+app.get("/favicon.ico", (req, res) => res.redirect("/cropped-logo-32x32.png"));
 
+//Simple CSRF Protected Pages
 const formPages = [
-  "login", "auth", "neu"
+  { name: "login", login: false },
+  { name: "auth", login: true },
+  { name: "neu", login: true }
 ];
-formPages.forEach(name => {
-  app.get("/" + name, (req, res) => {
-    res.render(name, { csrfToken: req.csrfToken() });
-  })
+formPages.forEach(page => {
+  const path = "/" + page.name;
+  if (page.login) app.get(path, authenticateJWT);
+  app.get(path, (req, res) => res.render(page.name, { csrfToken: req.csrfToken() }));
 });
 
+
+
+
+async function memberExists(vorname, nachname, geburt) {
+  const count = (await firestore
+    .collection("Members")
+    .where("vorname", "==", vorname)
+    .where("nachname", "==", nachname)
+    .where("geburt", "==", geburt)
+    .count().get())
+    .data().count;
+  return count > 0;
+}
+
+async function createMember(vorname, nachname, geburt, uvertrag, verein) {
+  return await firestore.collection("Members").add({
+    vorname: vorname,
+    nachname: nachname,
+    geburt: geburt,
+    uvertrag: uvertrag ?? false,
+    verein: verein ?? false
+  });
+}
+
+async function processData(data, res) {
+  const { vorname, nachname, uvertrag, verein } = data;
+  const geburt = Firestore.Timestamp.fromDate(new Date(data.geburt));
+
+  if (vorname && nachname && geburt) {
+    if (!await memberExists(vorname, nachname, geburt)) {
+      const result = await createMember(vorname, nachname, geburt, uvertrag, verein);
+      res.send(result);
+    } else {
+      res.statusCode = 422;
+      res.send("Member exists already");
+    }
+  }
+}
+
 app.post("/neu", authenticateJWT, verifyClaims("viewMembers"), (req, res) => {
-  console.dir(req.body);
-  res.send("OK");
-  //TODO transfer data into firestore
+  const { vorname, nachname, geburt, uvertrag, verein } = req.body;
+  processData(req.body, res);
 })
 
 app.get('/', (req, res) => {
@@ -101,11 +148,11 @@ app.post("/rights", authenticateJWT, (req, res) => {
   if (req.query.action) {
     switch (req.query.action) {
       case "promote":
-        getAuth().setCustomUserClaims(req.token.uid, { viewMembers: true, manageUsers: true });
+        auth.setCustomUserClaims(req.token.uid, { viewMembers: true, manageUsers: true });
         logout(req, res);
         break;
       case "demote":
-        getAuth().setCustomUserClaims(req.token.uid, null);
+        auth.setCustomUserClaims(req.token.uid, null);
         logout(req, res);
         break;
     }
@@ -119,20 +166,13 @@ app.post("/vote", authenticateJWT, verifyClaims('viewMembers'), (req, res) => {
 
 app.get("/users", authenticateJWT, verifyClaims('manageUsers'), (req, res) => {
   //TODO implement pagination if there are ever more than 50 users
-  getAuth().listUsers(50).then((result) => {
-    const userData = [];
-    result.users.map
-    result.users.forEach(userRecord => {
-      userRecord.customClaims ??= [];
-      userRecord.customClaims.manageUsers ??= false;
-      userRecord.customClaims.viewMembers ??= false;
-      userData.push({
-        name: userRecord.displayName,
-        email: userRecord.email,
-        viewMembers: userRecord.customClaims.viewMembers,
-        manageUsers: userRecord.customClaims.manageUsers
-      });
-    });
+  auth.listUsers(50).then((result) => {
+    const userData = result.users.map(userRecord => ({
+      name: userRecord.displayName,
+      email: userRecord.email,
+      viewMembers: userRecord.customClaims ? (userRecord.customClaims.viewMembers ?? false) : false,
+      manageUsers: userRecord.customClaims ? (userRecord.customClaims.manageUsers ?? false) : false
+    }));
     res.render("users", { users: userData });
   }).catch((error) => {
     console.log('Error listing users:', error);
@@ -143,11 +183,11 @@ app.get("/users", authenticateJWT, verifyClaims('manageUsers'), (req, res) => {
 app.post('/sessionLogin', (req, res) => {
   const idToken = req.body.idToken.toString();
 
-  getAuth().verifyIdToken(idToken).then((decodedIdToken) => {
+  auth.verifyIdToken(idToken).then((decodedIdToken) => {
     //Only accept recent id tokens to avoid stolen tokens
     if (new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60) {
       const expiresIn = 60 * 60 * 24 * 5 * 1000;
-      getAuth().createSessionCookie(idToken, { expiresIn }).then((sessionCookie) => {
+      auth.createSessionCookie(idToken, { expiresIn }).then((sessionCookie) => {
         const options = { maxAge: expiresIn, httpOnly: true, secure: true, sameSite: 'strict' };
         res.cookie('firebase-session', sessionCookie, options);
         res.status(200).end(JSON.stringify({ status: 'success' }));
@@ -164,10 +204,10 @@ app.post('/sessionLogin', (req, res) => {
 function logout(req, res) {
   const sessionCookie = req.cookies.session || '';
   res.clearCookie('firebase-session');
-  getAuth()
+  auth
     .verifySessionCookie(sessionCookie)
     .then((decodedClaims) => {
-      return getAuth().revokeRefreshTokens(decodedClaims.sub);
+      return auth.revokeRefreshTokens(decodedClaims.sub);
     })
     .then(() => {
       res.redirect('/login');
