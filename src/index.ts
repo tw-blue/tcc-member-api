74f10277c4c665b2705e18def3c715c142e48e4f
tw-blue <toniwalter.blue@gmail.com>
2024-09-21 18:39:45 +0200

    Import Assertion to Import Attribute

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 export * from './api/v1/openapi.js';
-
 export * from './constants/global.js';
 export * from './enums/club-member-state.js';
 export * from './enums/school-member-state.js';
-export { apiDoc };
+import apiDoc from './api/v1/openapi.json' with { type: 'json' };
 
-import apiDoc from './api/v1/openapi.json' assert { type: 'json' };
+export { apiDoc };
 
