b3bfa57fded7329ef0239880e438578bdf536fe7
tw-blue <toniwalter.blue@gmail.com>
2024-11-06 18:26:22 +0100

    Fix Package Setup

--- compile-api.ps1
+++ compile-api.ps1
@@ -1,4 +1,4 @@
 Get-ChildItem -File -Recurse -Filter *.json -Path src\api | ForEach-Object {
     Write-Host "Compiling $($_.FullName)"
-    npm exec oazapfts -- --optimistic $_.FullName ($_.FullName -replace '\.json', '.ts')
+    npx oazapfts -- --optimistic $_.FullName ($_.FullName -replace '\.json', '.ts')
 }
